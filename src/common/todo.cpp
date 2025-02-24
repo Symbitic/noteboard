64c4e3cb30cb5011ef8b9e7b802cfd8792df4241
Alex <alex.shaw.as@gmail.com>
2025-02-24 03:47:41 +0000

    chore(project): update devcontainer feature with clang

--- src/common/todo.cpp
+++ src/common/todo.cpp
@@ -2,3 +2,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 #include "todo.h"
+
+TODO::TODO(const QString &text, QObject *parent)
+    : QObject(parent)
+    , m_text(text)
+{}
--- src/common/todo.h
+++ src/common/todo.h
@@ -1,4 +1,21 @@
 // Copyright (C) 2025 Alex Shaw <alex.shaw.as@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-only
 
-#pragma once
+#ifndef TODO_H
+#define TODO_H
+
+#include <QString>
+
+//! [0]
+class TODO : public QObject
+{
+    Q_OBJECT
+
+public:
+    TODO(const QString &text, QObject *parent = nullptr);
+
+private:
+    QString m_text;
+};
+
+#endif
